14a1d5949089a9736c9b5f966c580b0c32a577d5
Amanda <jkim@lcs.on.ca>
2019-11-22 14:36:51 -0500

    Initial Commit

---
+++ SwiftApprentice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftApprentice
+//
+//  Created by Kim, Jenis on 2019-11-22.
+//  Copyright © 2019 Kim, Jenis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
